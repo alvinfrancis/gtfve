44530352692522721441e156a95dfb9c29f19cdd
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-04-02 22:59:48 +0800

    Add queries namespace

---
+++ src/clj/gtfve/data/queries.clj
@@ -0,0 +1,37 @@
+(ns gtfve.data.queries
+  (:require [datomic.api :as d]
+            [gtfve.data.connection :refer [conn uri feed]]))
+
+(defn trips []
+  (let [db (d/db conn)]
+    (->> (d/datoms db :aevt :trip/id)
+         (map :e)
+         (map (partial d/entity db))
+         (map d/touch))))
+
+(defn routes
+  ([]
+   (let [db (d/db conn)]
+     (->> (d/datoms db :aevt :route/id)
+          (map :e)
+          (map (partial d/entity db))
+          (map d/touch))))
+  ([id]
+   (let [db (d/db conn)]
+     (->> (d/q {:find ['?r]
+                :in ['$ '?id]
+                :where [['?r :route/id '?id]]}
+               db id)
+          (map first)
+          (map (partial d/entity db))
+          (map d/touch)))))
+
+(defn haversine
+  [{lon1 :longitude lat1 :latitude} {lon2 :longitude lat2 :latitude}]
+  (let [R 6372.8
+        dlat (Math/toRadians (- lat2 lat1))
+        dlon (Math/toRadians (- lon2 lon1))
+        lat1 (Math/toRadians lat1)
+        lat2 (Math/toRadians lat2)
+        a (+ (* (Math/sin (/ dlat 2)) (Math/sin (/ dlat 2))) (* (Math/sin (/ dlon 2)) (Math/sin (/ dlon 2)) (Math/cos lat1) (Math/cos lat2)))]
+    (* R 2 (Math/asin (Math/sqrt a)))))
