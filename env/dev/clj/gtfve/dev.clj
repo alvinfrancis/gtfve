0409d62606301e666262149aaf4418b32ffa8f4a
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-04-20 22:33:32 +0800

    Allow starting of server in dev namespace

--- env/dev/clj/gtfve/dev.clj
+++ env/dev/clj/gtfve/dev.clj
@@ -1,7 +1,9 @@
 (ns gtfve.dev
   (:require [cemerick.piggieback :as piggieback]
             [weasel.repl.websocket :as weasel]
-            [leiningen.core.main :as lein]))
+            [leiningen.core.main :as lein]
+            [gtfve.handler :refer [app]]
+            [ring.adapter.jetty :refer [run-jetty]]))
 
 (defn browser-repl []
   (piggieback/cljs-repl :repl-env (weasel/repl-env :ip "0.0.0.0" :port 9001)))
@@ -10,3 +12,6 @@
   (future
     (print "Starting figwheel.\n")
     (lein/-main ["figwheel"])))
+
+(defn start-server []
+  (run-jetty app {:port 3000 :join? false}))
