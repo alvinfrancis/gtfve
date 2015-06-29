cf2ed65e82c32cdfaaf61b47577b53813f378686
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-06-29 17:57:29 +0800

    Namespace for core garden css

---
+++ src/clj/gtfve/styles/core.clj
@@ -0,0 +1,8 @@
+(ns gtfve.styles.core
+  (:require [garden.def :refer [defstylesheet defstyles]]
+            [garden.units :refer [px]]))
+
+(defstyles main
+  [[:.site-header {}]
+   [:.side-panel {}]
+   [:.maps-panel {}]])
