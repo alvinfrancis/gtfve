63b2069267a4a5419499aae131dd092cec646090
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-07-01 10:30:30 +0800

    Add maps panel toolbar placeholder

--- src/clj/gtfve/styles/core.clj
+++ src/clj/gtfve/styles/core.clj
@@ -1,15 +1,23 @@
 (ns gtfve.styles.core
   (:require [garden.def :refer [defstylesheet defstyles]]
-            [garden.units :refer [px percent]]))
+            [garden.units :refer [px percent]]
+            [garden.color :as color :refer [rgba rgb hsl]]))
 
 (defstyles main
   [["@font-face" {:font-family "Roboto"
                   :src "url(\"../font/Roboto/Roboto-Regular.ttf\")"}]
    [:body {:padding-top (px 64)}]
    [:.side-panel-container {:padding-left 0
-                            :padding-right 0}]
+                            :padding-right 0
+                            :border :none
+                            :z-index 1020
+                            :box-shadow [[0 (px 1) (px 2) (rgba 0 0 0 0.3)]]}]
    [:.main-panel-container {:padding-left 0
                             :padding-right 0}]
    [:.side-panel {:background-color "#e3f2fd"
                   :padding (px 20)
-                  :height (percent 100)}]])
+                  :height (percent 100)}]
+   [:.maps-toolbar {:background-color "#1976d2"
+                    :min-height "48px"}]
+   [:.maps-panel {:height (percent 100)
+                  :background-color "#e3f2f5"}]])
--- src/cljs/gtfve/components/maps_panel.cljs
+++ src/cljs/gtfve/components/maps_panel.cljs
@@ -2,11 +2,19 @@
   (:require [om.core :as om]
             [sablono.core :as html :refer-macros [html]]))
 
+(defn maps-toolbar [_ _]
+  (reify
+    om/IDisplayName (display-name [_] "Maps Toolbar")
+    om/IRender
+    (render [_]
+      (html [:div.maps-toolbar]))))
+
 (defn maps-panel [_ _]
   (reify
     om/IDisplayName (display-name [_] "Maps Panel")
     om/IRender
     (render [_]
       (html [:div.maps-panel
+             (om/build maps-toolbar nil)
              [:div
               [:p "Maps Panel"]]]))))
