112c2d6fe9e679a4be48647ba3798a48c8958536
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-07-03 11:17:49 +0800

    Add controller component to bind with root
    This will handle events passed to comms channels. This allows an easy
    way to access the app-state at the root as a cursor and saves having
    to put the handler logic on separate from om.

---
+++ src/cljs/gtfve/components/controller.cljs
@@ -0,0 +1,41 @@
+(ns gtfve.components.controller
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]
+                   [gtfve.macros :refer [<?]])
+  (:require [om.core :as om]
+            [sablono.core :as html :refer-macros [html]]
+            [cljs.core.async :as async :refer [put! chan <! close!]]
+            [gtfve.controllers.controls :as controls-con]))
+
+(defn- control-handler [v app]
+  (controls-con/control-event (first v) (rest v) app))
+
+(defn controller [app owner]
+  (reify
+    om/IDisplayName (display-name [_] "Controller")
+    om/IInitState
+    (init-state [_]
+      {:kill-ch (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [kill-ch (om/get-state owner :kill-ch)
+            comms (om/get-shared owner [:comms])
+            controls-mult (:controls-mult comms)
+            controls-tap (chan)]
+        (async/tap controls-mult controls-tap)
+        (om/set-state! owner :controls-tap controls-tap)
+        (go-loop []
+          (let [[v c] (alts! [kill-ch controls-tap])]
+            (if (= c kill-ch)
+              :done
+              (do
+                (condp = c
+                  controls-tap (control-handler v app))
+                (recur))))
+          (async/untap controls-mult controls-tap))))
+    om/IWillUnmount
+    (will-unmount [_]
+      (let [{:keys [kill-ch controls-tap]} (om/get-state owner)]
+        (when kill-ch (put! kill-ch (js/Date.)))))
+    om/IRender
+    (render [_]
+      (html [:noscript]))))
