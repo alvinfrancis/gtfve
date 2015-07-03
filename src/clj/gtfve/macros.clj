319cdf706563332a583b11718ce9506e15767b62
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-07-03 20:49:42 +0800

    Add inspect macro

--- src/clj/gtfve/macros.clj
+++ src/clj/gtfve/macros.clj
@@ -1,5 +1,13 @@
 (ns gtfve.macros)
 
+(defn- inspect-1 [expr]
+  `(let [result# ~expr]
+     (js/console.info (str (pr-str '~expr) " => " (pr-str result#)))
+     result#))
+
+(defmacro inspect [& exprs]
+  `(do ~@(map inspect-1 exprs)))
+
 (defmacro <? [ch]
   `(let [e (async/<! ~ch)]
      (if (instance? js/Error e)
