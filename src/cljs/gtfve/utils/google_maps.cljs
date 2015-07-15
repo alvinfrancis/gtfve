48fb1e96eb940bdcfd7abb617b1c96289c357387
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-07-15 16:20:18 +0800

    More google maps protocol extension

--- src/cljs/gtfve/utils/google_maps.cljs
+++ src/cljs/gtfve/utils/google_maps.cljs
@@ -13,6 +13,9 @@
                            :mapTypeId (:ROADMAP map-types)
                            :zoom 15})
 (extend-type Maps.LatLng
+  IIndexed
+  (-nth [this n] (nth (seq this) n))
+  (-nth [this n not-found] (nth (seq this) n not-found))
   ILookup
   (-lookup [this key] (get (js->clj this) key))
   (-lookup [this key not-found] (or (get (js->clj this) key) not-found))
@@ -22,6 +25,9 @@
   (-js->clj [this options] {:lat (.lat this) :lng (.lng this)}))
 
 (extend-type Data.Point
+  IIndexed
+  (-nth [this n] (nth (seq (.get this)) n))
+  (-nth [this n not-found] (nth (seq (.get this)) n not-found))
   ILookup
   (-lookup [this key] (get (js->clj this) key))
   (-lookup [this key not-found] (or (get (js->clj this) key) not-found))
@@ -29,6 +35,22 @@
   (-seq [this] (seq (.get this)))
   IEncodeClojure
   (-js->clj [this options] (apply js->clj (.get this) options)))
+
+(extend-type Maps.LatLngBounds
+  IIndexed
+  (-nth [this n] (nth (seq this) n))
+  (-nth [this n not-found] (nth (seq this) n not-found))
+  ILookup
+  (-lookup [this key] (get {:sw (.getSouthWest this)
+                            :ne (.getNorthEast this)} key))
+  (-lookup [this key not-found] (or (get {:sw (.getSouthWest this)
+                                          :ne (.getNorthEast this)} key)
+                                    not-found))
+  ISeqable
+  (-seq [this] (list (.getSouthWest this) (.getNorthEast this)))
+  IEncodeClojure
+  (-js->clj [this options] {:sw (js->clj (.getSouthWest this))
+                            :ne (js->clj (.getNorthEast this))}))
 
 
 (defn data-listen
