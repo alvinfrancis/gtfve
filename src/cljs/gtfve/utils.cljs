b719d8e8094d3ba58b23d52e67a7b1cbae3a2501
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-08-04 23:24:38 +0800

    Add a deep-merge utility

--- src/cljs/gtfve/utils.cljs
+++ src/cljs/gtfve/utils.cljs
@@ -59,3 +59,10 @@
   "Same as js->clj, but keywordizes-keys by default"
   [ds]
   (js->clj ds :keywordize-keys true))
+
+(defn deep-merge
+  "Recursively merges maps. If vals are not maps, the last value wins."
+  [& vals]
+  (if (every? map? vals)
+    (apply merge-with deep-merge vals)
+    (last vals)))
